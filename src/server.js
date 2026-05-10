8658d9bb3721ab7529454befadbc4f3a487c53d1
Jai_Jayathilak <jaijayathilak@gmail.com>
2026-05-10 14:43:38 +0530

    setup neon db w/ drizzle

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env.development',
+});
+
 import app from './app.js';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
