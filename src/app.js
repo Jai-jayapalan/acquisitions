3caf8ee3dd1890fa714037120f6f9e7c06520df4
jai-jayapalan <jaijayathilak@gmail.com>
2026-05-17 22:59:16 +0530

    Implemented middlewares

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import logger from './config/logger.js';
 
 const app = express();
 
+// middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Aquisitions API!');
   res.status(200).json({ message: 'Hello from Aquisitions!' });
 });
 
