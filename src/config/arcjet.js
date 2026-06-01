5c78d2692c0c69a95102246bf466a8ab672e8fba
Jai-jayapalan <jaijayathilak@gmail.com>
2026-06-01 21:17:59 +0530

    Fixied test arcjet.js file

--- src/config/arcjet.js
+++ src/config/arcjet.js
@@ -1,6 +1,14 @@
 import arcjet, { shield, detectBot, slidingWindow } from '@arcjet/node';
 
-export const aj = arcjet({
+const isTest = process.env.NODE_ENV === 'test';
+
+export const aj = isTest ? {
+  withRule: () => ({
+      protect: async () => ({
+      isDenied: () => false,
+    }),
+  }),
+} : arcjet({
   key: process.env.ARCJET_KEY,
   rules: [
     shield({ mode: 'LIVE' }),
