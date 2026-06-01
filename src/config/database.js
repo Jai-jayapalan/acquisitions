5afab981e176259e7a27baa3022c04f4a02bfe01
Jai-jayapalan <jaijayathilak@gmail.com>
2026-06-01 20:35:37 +0530

    Implemented CI/CD pipelines and github actions

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,13 @@
 import { neon, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http'
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV == 'development' || process.env.NODE_ENV == 'test') {
     neonConfig.fetchEndpoint = 'http://neon-local:5432/sql';
     neonConfig.useSecureWebSocket = false;
     neonConfig.poolQueryViaFetch = true;
 }
+
+console.log(process.env.NODE_ENV)
 
 const sql = neon(process.env.DATABASE_URL);
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,8 +5,8 @@ import { authenticateToken, requireRole } from "#middlewares/auth.middleware.js"
 const userRouter = express.Router();
 
 userRouter.get('/', fetchAllUsers);
-userRouter.get('/:id', authenticateToken, getUserById);
-userRouter.put('/:id', authenticateToken, updateUser);
+userRouter.get('/:id', authenticateToken, fetchUserById);
+userRouter.put('/:id', authenticateToken, updateUserById);
 userRouter.delete('/:id', authenticateToken, requireRole(['admin']), deleteUserById);
 
 export default userRouter;
