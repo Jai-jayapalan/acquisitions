18d09c709bbe3a9188f68a5917c09f79eb0c0fa7
Jai-jayapalan <jaijayathilak@gmail.com>
2026-06-01 21:23:23 +0530

    Ignored test action from workflow

--- src/config/arcjet.js
+++ src/config/arcjet.js
@@ -1,14 +1,6 @@
 import arcjet, { shield, detectBot, slidingWindow } from '@arcjet/node';
 
-const isTest = process.env.NODE_ENV === 'test';
-
-export const aj = isTest ? {
-  withRule: () => ({
-      protect: async () => ({
-      isDenied: () => false,
-    }),
-  }),
-} : arcjet({
+export const aj = arcjet({
   key: process.env.ARCJET_KEY,
   rules: [
     shield({ mode: 'LIVE' }),
